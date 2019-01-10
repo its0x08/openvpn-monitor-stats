f684d07dc5e6127055ef8959b9bd7ca145887d17
its0x08 <its0x08@gmail.com>
2019-01-11 00:57:23 +0300

    v0.0-8

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 						</tr>
 					</thead>
 					<tbody><?php
+					error_reporting(0);
 					echo file_get_contents("stats.txt");
 					?>
 
--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-python /var/www/html/main.py & 
-iptables --flush;
-rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
-yum remove -y php-common  
-yum install -y php56w php56w-mysql php56w-common php56w-pdo php56w-opcache
-php -S 0.0.0.0:2028 &
+
+if [ `which apt-get` = "/usr/bin/apt-get" ]; then
+        echo Debian/Ubuntu;
+        apt update;
+        apt install -y php7.0;
+        python main.py &
+        iptables --flush;
+        sleep 3;
+        php -S 0.0.0.0:2028 &
+elif [`which yum` = "/usr/bin/apt-get"]; then
+        echo CentOS/RedHat;
+        python main.py &
+        iptables --flush;
+        rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm;
+        yum remove -y php-common;
+        yum install -y php56w php56w-mysql php56w-common php56w-pdo php56w-opcache;
+        sleep 3;
+        php -S 0.0.0.0:2028 &
+fi
