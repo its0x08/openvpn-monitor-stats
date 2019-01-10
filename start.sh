6bc6c923633af484599c762a3748ed206fbc4469
its0x08 <its0x08@gmail.com>
2019-01-10 13:27:18 +0300

    v0.0-2

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+cp ./index.php /var/www/html/;
+cp ./main.py /var/www/html/;
+cp ./stats.txt /vaw/www/html;
+iptables --flush;
+sudo yum install -y httpd php;
+sudo service httpd start;
+sudo chkconfig httpd on;
+sudo service httpd restart;
+
