0fa6fd753521eed3d9857929a4389d416e835f39
its0x08 <its0x08@gmail.com>
2019-01-10 15:03:18 +0300

    v0.0-8

--- start.sh
+++ start.sh
@@ -3,9 +3,5 @@ python /var/www/html/main.py &
 iptables --flush;
 rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
 yum remove -y php-common  
-yum install -y php56w
-yum install -y php56w-mysql
-yum install -y php56w-common
-yum install -y php56w-pdo
-yum install -y php56w-opcache
-php -S 0.0.0.0;8080 &
+yum install -y php56w php56w-mysql php56w-common php56w-pdo php56w-opcache
+php -S 0.0.0.0:8080 &
